b51fcb3d0906b4475a5eddcd7147caa972c87f50
sam-w-miller <sam@learnlatin.net>
2024-04-04 13:33:29 +0100

    Added credential reading

--- tvcabinet-datacollector.py
+++ tvcabinet-datacollector.py
@@ -1,2 +1,11 @@
 # Code to connect to Adafruit and download data
 
+import configparser
+
+config = configparser.ConfigParser()
+config.read("creds.config")
+
+username = config['Credentials']['username']
+key = config['Credentials']['key']
+
+print(username)
